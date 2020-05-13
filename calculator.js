ce46f6de6d354f0f62c67e2c47f50211edc162b7
Joao Gomes <joaopfg.2002@gmail.com>
2020-05-13 01:14:10 +0100

    Routes, and adding the numbers

--- calculator.js
+++ calculator.js
@@ -1,14 +1,29 @@
 //jshint esversion:6
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.get("/", function(req,res){
     
-    res.send("<h1>Hello World</h1>")
+  res.sendFile(__dirname + "/index.html");
+  
+});
+
+app.post("/" , function(req,res){
+
+   var num1 = Number(req.body.num1);
+   var num2 = Number(req.body.num2);
+
+   var result = num1 + num2;
+
+   res.send("O resultado da soma de " + num1 +  " com " + num2 + " é " + result);
+
 });
 
 
-app.listen(port);
+app.listen(3000);
